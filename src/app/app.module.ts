3e282693a5211b00631df532097ebc595cbf1369
LangPN <langpn.dev@gmail.com>
2024-09-17 22:45:28 +0700

    Splitting the App Into Feature Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,9 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {PostListComponent} from './posts/post-list/post-list.component';
-import {PostCreateComponent} from './posts/post-create/post-create.component';
 import {HeaderComponent} from './header/header.component';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {FormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule, provideHttpClient} from "@angular/common/http";
 import {SignupComponent} from './auth/signup/signup.component';
 import {LoginComponent} from './auth/login/login.component';
@@ -14,12 +12,11 @@ import {AuthInterceptor} from "./auth/auth-interceptor";
 import {ErrorInterceptor} from "./error-interceptor";
 import {ErrorComponent} from './error/error.component';
 import {AngularMaterialModule} from "./angular-material.module";
+import {PostsModule} from "./posts/posts.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent,
-    PostCreateComponent,
     HeaderComponent,
     SignupComponent,
     LoginComponent,
@@ -29,9 +26,9 @@ import {AngularMaterialModule} from "./angular-material.module";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    AngularMaterialModule
+    AngularMaterialModule,
+    PostsModule,
   ],
   providers: [
     {
---
+++ src/app/posts/posts.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from "@angular/core";
+import {PostListComponent} from "./post-list/post-list.component";
+import {PostCreateComponent} from "./post-create/post-create.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {AngularMaterialModule} from "../angular-material.module";
+import {CommonModule} from "@angular/common";
+
+@NgModule({
+  declarations: [
+    PostCreateComponent,
+    PostListComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    RouterModule
+  ],
+})
+export class PostsModule {
+
+}
